bf14ce135229ed0932451ed92b6851ea2eff035d
yshgt <yshgoto@gmail.com>
2017-01-15 16:44:19 +0900

    first

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// var console = { log: function() {} }; // comment out for production mode
+
+function Main() {
+  this.init();
+}
+
+Main.prototype.init = function() {
+};
+
+window.onload = function() {
+};
